cd7374ee7894d0afecf269eb5624ba4b59011a1d
AW <idmese@gmail.com>
2025-09-23 11:59:43 +0300

    Initial Commit

---
+++ FlightDealsAggregator/FlightDealsAggregatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightDealsAggregatorApp.swift
+//  FlightDealsAggregator
+//
+//  Created by Андрей Андриянов on 23.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlightDealsAggregatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
